eea96cfec1aa7cad4190e337784161d619c0e3fd
tasyamalia <tasyaamaliasalsa@gmail.com>
2024-11-10 20:43:00 +0700

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
